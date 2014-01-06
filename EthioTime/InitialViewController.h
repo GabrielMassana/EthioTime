7e9b54a84801dac4758d0acf497fa34390fc7dc5
Gabriel Massana <gabriel.massana@irisvalet.com>
2014-01-06 13:17:58 +0000

    Initial Commit

---
+++ EthioTime/InitialViewController.h
@@ -0,0 +1,13 @@
+//
+//  InitialViewController.h
+//  EthioTime
+//
+//  Created by Gabriel Massana on 1/6/14.
+//  Copyright (c) 2014 Gabriel Massana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InitialViewController : UIViewController
+
+@end
---
+++ EthioTime/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EthioTime
+//
+//  Created by Gabriel Massana on 1/6/14.
+//  Copyright (c) 2014 Gabriel Massana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "InitialAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([InitialAppDelegate class]));
+    }
+}
